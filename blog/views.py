1bf327de18f5c4648552d13131ae5e998822c63f
srinidhi <kss682@gmail.com>
2018-07-29 19:41:08 +0530

    updated template

--- blog/views.py
+++ blog/views.py
@@ -11,4 +11,6 @@ def post_detail(request,pk):
 	post=get_object_or_404(Post,pk=pk)
 	return render(request,'blog/post_detail.html',{'post':post})
 
-
+def post_new(request):
+	form=PostForm()
+	return render(request,'blog/post_edit.html',{'form':form})
