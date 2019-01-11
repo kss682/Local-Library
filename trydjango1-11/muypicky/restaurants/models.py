1a430b04f41ba5782ba282a299bac5d916212e8a
srinidhi <kss682@gmail.com>
2019-01-11 23:15:09 +0530

    udemy course

--- trydjango1-11/muypicky/restaurants/models.py
+++ trydjango1-11/muypicky/restaurants/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name        =   models.CharField(max_length = 200)
+    location    =   models.CharField(max_length = 200,null = True,blank = True)
