780fc2dd19b25675dcf8205827867be7b4000240
kss682 <kss682@gmail.com>
2020-01-07 00:31:35 +0530

    removed old proj

--- trydjango1-11/muypicky/restaurants/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Restaurant(models.Model):
-    name        =   models.CharField(max_length = 200)
-    location    =   models.CharField(max_length = 200,null = True,blank = True)
